10daf63d62c5c668a45cc3921132f60631733974
muri <99483672+murichristopher@users.noreply.github.com>
2022-12-04 22:41:07 -0300

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,21 +1,21 @@
-# frozen_string_literal: true
+      # frozen_string_literal: true
 
-class HomeController < ApplicationController
-  before_action :set_view_variables, only: %i[index]
+      class HomeController < ApplicationController
+        before_action :set_view_variables, only: %i[index]
 
-  def index; end
+        def index; end
 
-  def profile; end
+        def profile; end
 
-  def set_view_variables
-    return unless current_user
+        def set_view_variables
+          return unless current_user
 
-    @users_to_chat_with = User.all_except_contacts_of(current_user)
-    @contacts = current_user.contacts
-    @rooms = current_user.rooms
-  end
+          @users_to_chat_with = User.all_except_contacts_of(current_user)
+          @contacts = current_user.contacts
+          @rooms = current_user.rooms
+        end
 
-  def teste
-    @teste = 'teste'
-  end
-end
+        def teste
+          @teste = 'teste'
+        end
+      end
