c60f31040fbc56591a2819fec6fc9d8302c62d72
muri <99483672+murichristopher@users.noreply.github.com>
2022-12-04 22:09:48 -0300

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,21 +1,21 @@
-    class HomeController < ApplicationController
-      before_action :set_view_variables, only: %i[index]
+            class HomeController < ApplicationController
+              before_action :set_view_variables, only: %i[index]
 
 
 
-              def index; 
-              end
+                      def index; 
+                      end
 
 
-              def profile; 
-                end
+                      def profile; 
+                        end
 
-                          def teste
-                              @oi = 'teste'
-                  end
+                                  def teste
+                                      @oi = 'teste'
+                          end
 
-                      def set_view_variables
-                      return unless current_user
+                              def set_view_variables
+                              return unless current_user
 
                           @users_to_chat_with = User.all_except_contacts_of(current_user)
                     @contacts = current_user.contacts
