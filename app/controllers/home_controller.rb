f02999f1dc5df98d807cc98229583c2411819ec2
muri <99483672+murichristopher@users.noreply.github.com>
2022-12-04 22:20:18 -0300

    testando

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,21 +1,21 @@
 class HomeController < ApplicationController
-  before_action :set_view_variables, only: %i[index]
-
-  
-
-  def index; 
-  end
+        before_action :set_view_variables, only: %i[index]
 
 
-  def profile; 
-  end
 
-  def set_view_variables
-    return unless current_user
+        def index; 
+        end
 
-    @users_to_chat_with = User.all_except_contacts_of(current_user)
-    @contacts = current_user.contacts
-    @rooms = current_user.rooms
+
+        def profile; 
+        end
+
+        def set_view_variables
+          return unless current_user
+
+          @users_to_chat_with = User.all_except_contacts_of(current_user)
+          @contacts = current_user.contacts
+          @rooms = current_user.rooms
   end
 
   
